0c6b725cc603363acca0c640cccc4a9d443ab305
Josh Wedekind <josh@halfnibble.com>
2024-02-24 14:03:48 -0800

    Setup books api

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 // DEPENDENCIES
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to mongo: ', process.env.MONGO_URI);
+});
 
 // MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to the Hello World! API')
-})
+    res.send('Welcome to the Books API!');
+});
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('Greetings! From port: ', PORT);
-})
+    console.log('Greetings! From port: ', PORT);
+});
